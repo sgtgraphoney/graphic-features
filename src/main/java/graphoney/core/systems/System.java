f4f462df84d5a586cf67e68f37a94a3698ced328
Arseniy Buravlyov <ars.dr@yandex.ru>
2017-03-22 05:46:23 +0300

    Documentation for system classes was added

--- src/main/java/graphoney/core/systems/System.java
+++ src/main/java/graphoney/core/systems/System.java
@@ -2,48 +2,89 @@ package graphoney.core.systems;
 
 import graphoney.utils.logging.Logger;
 
+/**
+ * <p>Represents the functional system of engine. Each implementation of system must extend this class.</p>
+ * <p>The system works in its own thread, that is an instance of {@link SystemThread}.</p>
+ */
 public abstract class System {
 
     private String name;
     private SystemThread thread;
 
+    /**
+     * Constructs a new system with specified parameters.
+     * @param name the name of the system.
+     * @param action the action that must be performed cyclically on each frame.
+     */
     public System(String name, FrameAction action) {
         this.name = name;
         thread = new SystemThread(action);
     }
 
+    /**
+     * Starts the thread of the system.
+     */
     public void start() {
         Logger.printInfo("Starting system '" + name + "'...");
         thread.start();
     }
 
+    /**
+     * Suspends execution of the frame action.
+     * Does not interrupts the thread.
+     */
     public void pause() {
         Logger.printInfo("Pausing system '" + name + "'...");
         thread.setActive(false);
     }
 
+    /**
+     * Resumes execution of the frame action.
+     * Does not resumes the thread.
+     */
     public void resume() {
         Logger.printInfo("Resuming system '" + name + "'...");
         thread.setActive(true);
     }
 
+    /**
+     * Interrupts the thread of the system and stops the system.
+     */
     public void terminate() {
         Logger.printInfo("Terminating system '" + name + "'...");
         thread.interrupt();
     }
 
+    /**
+     * Returns the name of the current system.
+     * @return the name of the system.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the boolean value that indicates whether the system runs.
+     * @return the value of the <tt>active</tt> flag.
+     */
     public boolean isActive() {
         return thread.isActive();
     }
 
+    /**
+     * Returns a boolean value that indicates whether the system is terminated and the thread is not alive.
+     * @return a boolean value that indicates whether the system is terminated.
+     */
     public boolean isTerminated() {
         return !thread.isAlive();
     }
 
+    /**
+     * Transfers any data to the current system.
+     * This is the only way to transfer data from one system to another.
+     * @param data data that is required for the system.
+     * @throws IllegalArgumentException if the data has illegal type for the current system.
+     */
     public abstract void transferData(Object data) throws IllegalArgumentException;
 
 }
--- src/main/java/graphoney/core/systems/SystemThread.java
+++ src/main/java/graphoney/core/systems/SystemThread.java
@@ -1,14 +1,33 @@
 package graphoney.core.systems;
 
+/**
+ * Represents the thread that the system of engine uses to run.
+ */
 public class SystemThread extends Thread {
 
     private FrameAction target;
-    private boolean active = true;
+    private boolean active;
 
+    /**
+     * Constructs a new thread for functional system of engine.
+     * @param target the action that must be performed cyclically on each frame.
+     */
     public SystemThread(FrameAction target) {
         this.target = target;
     }
 
+    /**
+     * Starts the thread.
+     */
+    @Override
+    public synchronized void start() {
+        active = true;
+        super.start();
+    }
+
+    /**
+     * Cyclically performs the action until the thread is interrupted.
+     */
     @Override
     public void run() {
         while (!isInterrupted()) {
@@ -22,10 +41,18 @@ public class SystemThread extends Thread {
         }
     }
 
+    /**
+     * Returns the boolean value that indicates whether the action is performed.
+     * @return the value of the <tt>active</tt> flag.
+     */
     public boolean isActive() {
         return active;
     }
 
+    /**
+     * Sets the <tt>active</tt> flag to the specified value;
+     * @param active a new value of the flag.
+     */
     public void setActive(boolean active) {
         this.active = active;
     }
